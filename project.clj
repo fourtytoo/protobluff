42cdc44e931bbb9879db6acc29058c162656f934
Walter Pelissero <wcp@book.home.lan>
2021-11-27 14:09:28 +0100

    Add protobuf files
    Update dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,22 @@
   :url "http://github.com/fourtytoo/protobluff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.google.protobuf/protobuf-java "3.15.1"]
-                 [io.grpc/grpc-core "1.35.0"]
-                 [io.grpc/grpc-netty "1.35.0"
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.google.protobuf/protobuf-java "3.19.1"]
+                 [io.grpc/grpc-core "1.42.1"]
+                 [io.grpc/grpc-netty "1.42.1"
                   :exclusions [io.grpc/grpc-core
                                io.netty/netty-codec-http2]]
-                 [io.grpc/grpc-protobuf "1.35.0"]
-                 [camel-snake-kebab "0.4.0"]]
-  :repl-options {:init-ns protobluff.core})
+                 [io.grpc/grpc-protobuf "1.42.1"]
+                 [camel-snake-kebab "0.4.2"]]
+  :repl-options {:init-ns protobluff.core}
+  :plugins [[lein-protoc "0.5.0"]]
+  ;; overridden in other profiles
+  :proto-source-paths []
+  :proto-target-path "target/generated-sources/protobuf"
+  :profiles {:dev {:proto-source-paths ["src/proto"]
+                   :java-source-paths  ["target/generated-sources/protobuf"]
+                   :dependencies [[com.taoensso/tufte "2.2.0"]
+                                  [io.grpc/grpc-stub "1.42.1"]
+                                  [io.grpc/grpc-netty "1.42.1" :exclusions [io.grpc/grpc-core]]
+                                  [io.netty/netty-tcnative-boringssl-static "2.0.40.Final"]]}})
